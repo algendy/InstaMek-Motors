8b7b2db52c69a0a14ec7bdfc531cc6db43f2ba09
Mohammad <mohammad@mainlineinnovations.com>
2016-01-14 14:35:05 -0700

    Positron beta

---
+++ InstaMek_Motors.Module.Web/WebModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using System.Collections.Generic;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.ExpressApp.Model.Core;
+using DevExpress.ExpressApp.Model.DomainLogics;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+
+namespace InstaMek_Motors.Module.Web {
+    [ToolboxItemFilter("Xaf.Platform.Web")]
+
+    public sealed partial class InstaMek_MotorsAspNetModule : ModuleBase {
+        public InstaMek_MotorsAspNetModule() {
+            InitializeComponent();
+        }
+        public override IEnumerable<ModuleUpdater> GetModuleUpdaters(IObjectSpace objectSpace, Version versionFromDB) {
+            return ModuleUpdater.EmptyModuleUpdaters;
+        }
+        public override void Setup(XafApplication application) {
+            base.Setup(application);
+            // Manage various aspects of the application UI and behavior at the module level.
+        }
+    }
+}
